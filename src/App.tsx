b343ff3fd85c52373f82d4ca1ab5aa8357ffc33e
Orealba Soriano <orealbas@gmail.com>
2024-11-15 12:53:56 +0100

    creating the card

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 
 import './App.css';
 import Card from './components/card';
 
+interface TutorialData {
+  image: string;
+  title: string;
+  description: string;
+  bgColor: string;
+}
 function App() {
-  const [steps, setSteps] = useState<number>(0);
-  let tutorialData:Array <any>= [
-  
+  // const [step, setStep] = useState<number>(0);
+  let tutorialData: TutorialData[] = [
     {
-      title: "Dedica muchas horas",
-      description: "Un mínimo de 30 horas a la semana. Si no tienes suficiente, tendrás que dedicarle más horas. Al principio parece imposible, pero notarás una mejoría rápidamente.",
-      bgColor:"",
-      Image:"../public/images/Time Management.svg"
-
+      title: 'Dedica muchas horas',
+      description:
+        'Un mínimo de 30 horas a la semana. Si no tienes suficiente, tendrás que dedicarle más horas. Al principio parece imposible, pero notarás una mejoría rápidamente.',
+      bgColor: '#2596be',
+      image: '../public/images/Time Management.svg',
     },
     {
-      title: "Programa proyectos propios",
-      description: "Mejor 10 horas trabajando en proyectos propios, que 10 horas mirando tutoriales. La motivación y la implicación en el proyecto ayudará a acelerar tu aprendizaje.",
-      bgColor:"",
-      Image:"../public/images/Programming (1).svg",
-
+      title: 'Programa proyectos propios',
+      description:
+        'Mejor 10 horas trabajando en proyectos propios, que 10 horas mirando tutoriales. La motivación y la implicación en el proyecto ayudará a acelerar tu aprendizaje.',
+      bgColor: '',
+      image: '../public/images/Programming (1).svg',
     },
     {
-      title: "Intenta descansar",
-      description: "Descansar bien y desconectar es vital. De esta forma reducirás el estrés y la ansiedad. Mejorarás tu concentración y consolidarás tu aprendizaje.",
-      bgColor:"",
-      Image:"../public/images/Meditation (1).svg"
-
-    }
-
-  ]
+      title: 'Intenta descansar',
+      description:
+        'Descansar bien y desconectar es vital. De esta forma reducirás el estrés y la ansiedad. Mejorarás tu concentración y consolidarás tu aprendizaje.',
+      bgColor: '',
+      image: '../public/images/Meditation (1).svg',
+    },
+  ];
   return (
     <>
-      <Card />
+      <Card
+        image={tutorialData[0].image}
+        title={tutorialData[0].title}
+        description={tutorialData[0].description}
+        bgColor={tutorialData[0].bgColor}
+      />
     </>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,38 @@
-function Card(steps) => {
-    return 
+import './card.css';
+
+export default function Card({
+  title,
+  description,
+  bgColor,
+  image,
+}: {
+  title: any;
+  description: any;
+  bgColor: any;
+  image: any;
+}): JSX.Element {
+  return (
+    <div>
+      {/* card */}
+      <div>
+        <div className={`max-w-sm bg-white border border-gray-200 rounded-lg shadow ${bgColor}`}>
+          <a href="#">
+            <img
+              className="rounded-t-lg "
+              src={image}
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                {title}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Card;
